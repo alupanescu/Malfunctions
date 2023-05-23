190976b4803a554e77d70ce019a827f64bc6a926
alupanescu <alupanescu@gmail.com>
2023-05-23 19:22:16 +0300

    commit

--- Malfunctions/settings.py
+++ Malfunctions/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = (os.path.join(BASE_DIR), 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -131,3 +132,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- report_a_malfunction/models.py
+++ report_a_malfunction/models.py
@@ -6,6 +6,9 @@ class Malfunction(models.Model):
     name= models.CharField(max_length=200, null=False)
     location=models.CharField(max_length=300, null=False)
     explanation=models.CharField(max_length=500, null=False)
+    img_upload = models.ImageField(upload_to='img/', null=True)
+    file_upload = models.FileField(upload_to='files/', null=True)
+
 
 
 
